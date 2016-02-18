bdaa9b6c28e876d308eb94c6076171dcfb2a245c
Mars <851104757@qq.com>
2016-02-18 17:23:20 +0800

    archive

---
+++ Project/UnityProject/Assets/Scripts/Archive/Protobuf/ProtobufMng.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class ProtobufMng : MonoBehaviour
+{
+    private ProtobufSerializer m_serializer = new ProtobufSerializer();
+    private static ProtobufMng s_instance = null;
+
+    void Awake()
+    {
+        s_instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public static ProtobufMng instance
+    {
+        get { return s_instance; }
+    }
+
+    public ProtobufSerializer Serializer
+    {
+        get { return m_serializer; }
+    }
+}
---
+++ Project/UnityProject/Assets/Scripts/Archive/Protobuf/ProtobufSerializeUtility.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+public static class ProtobufSerializeUtility 
+{
+    public static string SerializeObjectToStr(object obj)
+    {
+        byte[] bytesArray = SerializeObject(obj);
+        return bytesArray.ToString();
+    }
+
+    public static T DeserializeObjectByStr<T>(string sourcStr)
+    {
+        byte[] bytesArray = System.Text.Encoding.Default.GetBytes(sourcStr);
+        return DeserializeObject<T>(bytesArray);
+    }
+
+    public static byte[] SerializeObject(object obj)
+    {
+        byte[] bodys;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            ProtobufMng.instance.Serializer.Serialize(ms, obj);
+            bodys = new byte[ms.Length];
+            ms.Seek(0, SeekOrigin.Begin);
+            ms.Read(bodys, 0, bodys.Length);
+            ms.Close();
+        }
+        return bodys;
+    }
+
+    public static T DeserializeObject<T>(byte[] bodys)
+    {
+        T t;
+        using (MemoryStream ms = new MemoryStream(bodys))
+        {
+            t = (T)ProtobufMng.instance.Serializer.Deserialize(ms, null, typeof(T));
+            ms.Close();
+        }
+        return t;
+    }
+
+}
